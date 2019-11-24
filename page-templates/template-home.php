0d25d25c8ef5e53c0a1a6aa6daa533f2b5cf5d65
Mihailo Pantovic <mihailo.pantovic@helbiz.com>
2019-11-24 14:04:59 +0100

    ADD: article section to home page

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -43,7 +43,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="home-section align-center">
-                    <img src="https://placehold.it/50x50" alt="">
+                    <img class="centered-image" src="https://placehold.it/50x50" alt="">
                     <h2>O meni</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quo dolores tempora eum quasi sint odit hic nemo fugit, placeat quis eaque exercitationem dignissimos deserunt. Eum aliquid delectus non voluptatibus?</p>
                     <a href="#" class="button">Pročitaj više</a>
@@ -56,12 +56,40 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <div class="home-section align-center">
-                    <img src="https://placehold.it/50x50" alt="">
-                    <h2>Pravo iz mog mastila</h2>
+                <div class="home-section">
+                    <img class="image-center" src="https://placehold.it/50x50" alt="">
+                    <h2 class="align-center">Pravo iz mog mastila</h2>
                     <div class="row">
-                        <div class="col-lg-6"><img src="https://placehold.it/800x600" alt=""></div>
-                        <div class="col-lg-6"><img src="https://placehold.it/800x600" alt=""></div>
+                        <div class="col-lg-6">
+                            <div class="article">
+                                <img src="https://placehold.it/800x600" alt="">
+                                <p class="article-category">u <a href="#">Proza</a></p>
+                                <h2 class="article-title">Lorem ipsum dolor sit amet consectetur.</h2>
+                                <p class="article-excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error explicabo voluptatibus voluptates molestias doloremque enim soluta officiis iste omnis provident!</p>
+                                <a href="#" class="read-more">Pročitaj više</a>
+                                <ul class="article-social-share list-reset">
+                                    <li><a href="#"><i class="icon-facebook"></i></a></li>
+                                    <li><a href="#"><i class="icon-twitter"></i></a></li>
+                                    <li><a href="#"><i class="icon-instagram"></i></a></li>
+                                    <li><a href="#"><i class="icon-tumblr"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="article">
+                                <img src="https://placehold.it/800x600" alt="">
+                                <p class="article-category">u <a href="#">Proza</a></p>
+                                <h2 class="article-title">Lorem ipsum dolor sit amet consectetur.</h2>
+                                <p class="article-excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error explicabo voluptatibus voluptates molestias doloremque enim soluta officiis iste omnis provident!</p>
+                                <a href="#" class="read-more">Pročitaj više</a>
+                                <ul class="article-social-share list-reset">
+                                    <li><a href="#"><i class="icon-facebook"></i></a></li>
+                                    <li><a href="#"><i class="icon-twitter"></i></a></li>
+                                    <li><a href="#"><i class="icon-instagram"></i></a></li>
+                                    <li><a href="#"><i class="icon-tumblr"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
