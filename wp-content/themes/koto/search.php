3b5585cc829a20e3704f29c6c34f25b31854792e
Mica <pantovic.mihailo@gmail.com>
2020-10-06 07:35:07 +0200

    FIX: search string

--- wp-content/themes/koto/search.php
+++ wp-content/themes/koto/search.php
@@ -3,7 +3,9 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="align-center">
-                <?php echo sprintf( __( '%s Search Results for ', 'wp_theme' ), $wp_query->found_posts ); echo get_search_query(); ?>
+                <!-- TODO: sometime make this normal -->
+                <?php // echo sprintf( __( '%s Search Results for ', 'wp_theme' ), $wp_query->found_posts ); echo get_search_query(); ?>
+                <?php echo sprintf( __( 'Pronađeni su sledeći rezultati za: ', 'wp_theme' ), $wp_query->found_posts ); echo get_search_query(); ?>
             </h1>
 
 
