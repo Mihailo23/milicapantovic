0bcba61e9a4d68cc93cbd307ac40443c612f7758
Mica <pantovic.mihailo@gmail.com>
2020-10-24 20:24:39 +0200

    FIX: remove ga

--- wp-content/themes/koto/header.php
+++ wp-content/themes/koto/header.php
@@ -12,23 +12,6 @@
     <!-- fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400|Scope+One&display=swap&subset=latin-ext"
         rel="stylesheet">
-    <!-- google analytics -->
-    <script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-176039449-1']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') +
-            '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    })();
-    </script>
-
     <?php wp_head(); ?>
 
 </head>
